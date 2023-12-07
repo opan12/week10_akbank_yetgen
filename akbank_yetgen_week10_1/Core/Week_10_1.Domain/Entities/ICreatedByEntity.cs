c2c2d3523aa5d7d8393e0c5d97f2428b67f438c4
SUDE NUR OPAN <114245040+opan12@users.noreply.github.com>
2023-12-07 19:36:34 +0300

    deleted

--- akbank_yetgen_week10_1/Core/Week_10_1.Domain/Entities/ICreatedByEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Week_10_1.Domain.Entitiies
-{
-    public interface ICreatedByEntity
-    {
-        public string CreatedByUserId { get; set; }
-        public DateTimeOffset CreatedOn { get; set; }
-
-    }
-}
